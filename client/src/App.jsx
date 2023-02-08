9983bcae75a34cce4f35105ffa51e532cc936330
Prashant <prashantd049@gmail.com>
2023-02-08 21:02:29 +0530

    Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div>
+
+      <div>header</div>
       
+
+
+      <div>body</div>
+
+
+
+      <div>input</div>
     </div>
   )
 }
