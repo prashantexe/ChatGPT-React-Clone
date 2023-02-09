8fd5d4c68ffa57f8e7df3fe84871a9a6a64411c0
Prashant <prashantd049@gmail.com>
2023-02-09 21:13:32 +0530

    Commits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div classname = 'bg-[#1A232E] h-screen py-6 relative sm:px-28'> 
+    <div classname = 'bg-[#1A232E] h-screen py-6 relative sm:px-28 text-white overflow-hidden flex flex-col justify-between align-middle'> 
 
       <div>header</div>
       
