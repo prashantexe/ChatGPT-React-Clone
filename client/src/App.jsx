b03f62aebc14cb94a5f92562d9d93844d18641d9
Prashant <prashantd049@gmail.com>
2023-02-09 21:22:39 +0530

    Commits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,13 @@ function App() {
   return (
     <div classname = 'bg-[#1A232E] h-screen py-6 relative sm:px-28 text-white overflow-hidden flex flex-col justify-between align-middle'> 
 
-      <div>header</div>
+      <div className="gradient-01 z-0 absolute"></div>
+      <div className="gradient-02 z-0  absolute"></div>    
+      
+      
+      <div className="upppercase font-bold text-2xl text-center mb-3">
+        ChatGpt 2.0
+      </div>
       
 
 
