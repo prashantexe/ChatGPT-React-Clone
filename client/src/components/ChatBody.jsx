836edf2c11695dff5a4dd68ac6ca5be224b6cb74
Prashant <prashantd049@gmail.com>
2023-02-11 19:26:23 +0530

    Updated

--- client/src/components/ChatBody.jsx
+++ client/src/components/ChatBody.jsx
@@ -6,7 +6,7 @@ return(
     <div className="flex flex-col gap-4">
     {/* client Measage */}
     <div className ="border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%]">
-        <pre className=''>
+        <pre className='whitespace-pre-wrap'>
             <span>Hey ChatGPT, Can you help me?</span>
         </pre>
     </div>
