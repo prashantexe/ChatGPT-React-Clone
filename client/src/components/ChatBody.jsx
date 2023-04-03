e7b2da4e3f7603c9300b3dbc5fe91bd5fa0f9bdb
Prashant <111880621+prashantexe@users.noreply.github.com>
2023-04-03 21:58:26 +0530

    Update ChatBody.jsx

--- client/src/components/ChatBody.jsx
+++ client/src/components/ChatBody.jsx
@@ -6,14 +6,14 @@ const ChatBody = () => {
 
 return(
     <div className="flex flex-col gap-4">
-    {/* client Measage */}
+    
     <div className ="border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%]">
         <pre className='whitespace-pre-wrap'>
             <span>Hey ChatGPT, Can you help me?</span>
         </pre>
     </div>
 
-        {/* ai Measage */}
+        
     <div className="border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%]">
         <pre className='whitespace-pre-wrap ${aiStyle}'>
             <span>Yeah, I can Help you with anything you want!!</span>
